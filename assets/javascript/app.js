0ddd03adfb635b13a9709d884e6a1009fb6ab05a
Austin Sharman <asharman22@gmail.com>
2018-11-15 05:24:59 -0500

    create game object

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+var game = {
+    currentQuestion: 1,
+    wrongAnswer: 0,
+    rightAnswer: 0,
+    answerGuessed: "",
+    
+    questions: [
+        {
+            question: "What is the name of the boat Link travels with in The Legend of Zelda Wind Waker?",
+            answer: "The King of Red Lions",
+            wrong1: "The Black Pearl",
+            wrong2: "S.S. Linebeck",
+            wrong3: "Triforce of Courage"
+        },
+        {
+            question: "Who is the Oracle of Secrets?",
+            answer: "Farore",
+            wrong1: "Din",
+            wrong2: "Nayru",
+            wrong3: "Zelda",
+        },
+        {
+            question: "Which Nintendo character can be found on the Happy Mask Salesman's bag?",
+            answer: "Mario",
+            wrong1: "Pikachu",
+            wrong2: "Donkey Kong",
+            wrong3: "Samus",
+        }
+    ],
+}
+
